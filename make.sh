f5ea76e7ae0c22ff3afa72d06dda633c29d6f654
Jon Sharp <sharpjon@gmail.com>
2022-08-01 21:16:36 -0500

    Working demo for pcd68's in-progress keyboard cont
     - Fixed memory map for flat model
     - Added interrupt vectors, etc.
     - Very basic keyboard input working, as interrupt handler

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-m68k-unknown-elf-gcc -Os -s -g -o pcd68home pcd68home.S -nostdlib -fomit-frame-pointer -lc -lm -mno-rtd -m68000 -msoft-float -mpcrel -T pcd68.lds
+m68k-unknown-elf-gcc -O0 -s -g -o pcd68home pcd68home.S -nostdlib -fomit-frame-pointer -mno-rtd -m68000 -msoft-float -mpcrel -T pcd68.lds
 
 m68k-unknown-elf-objcopy -O binary pcd68home program.bin
 m68k-unknown-elf-objcopy -O ihex pcd68home program.hex
